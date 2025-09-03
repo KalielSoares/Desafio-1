09ac0e280382854a878a819e63d902e7499b5a9b
kaliel <kalielsoares6@gmail.com>
2025-09-03 10:48:25 -0300

    Resolução Desafio 1

---
+++ Models/produtos.cs
@@ -0,0 +1,26 @@
+namespace Produto.Models;
+
+public class produtos
+{
+    private string _Nome { get; set; }
+    private int _Value = 15;
+
+    public produtos(string Nome, int Value)
+    {
+        _Nome = Nome;
+        _Value = Value;
+    }
+
+    public void ChangeValue(int n)
+    {
+        if (n > 0)
+            _Value = n;
+        else
+            Console.WriteLine("Valor inferior a 0");
+    }
+
+    public string GetData()
+    {
+        return $"Produto:{_Nome}, Preço:{_Value}";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Produto.Models;
+
+namespace Produto;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var Caderno = new produtos("Caderno", 15);
+        Console.WriteLine(Caderno.GetData());
+        
+        Caderno.ChangeValue(-12);
+        Console.WriteLine(Caderno.GetData());
+    }
+}
